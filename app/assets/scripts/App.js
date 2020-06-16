261d7629d89a43ab34f989526c10c6dfd09322ee
Michal <michal.kemblowski@gmail.com>
2020-06-16 09:50:45 +0200

    First Commit

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,8 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/mobile-menu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader'
+import StickyHeader from './modules/StickyHeader';
+import "lazysizes";
 
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
