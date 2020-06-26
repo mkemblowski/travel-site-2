cea02e1ac899dae66b378961e0f43597d270c2a3
Michal <michal.kemblowski@gmail.com>
2020-06-26 08:16:31 +0200

    asdf

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,8 +4,6 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import "lazysizes";
 
-alert("testasdfaing");
-
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
